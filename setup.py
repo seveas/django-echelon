5cc9896a9ce756aed9cd817f9f1cc46f16fff0f8
Dennis Kaarsemaker <dennis@kaarsemaker.net>
2010-02-13 17:16:30 +0100

    Initial public version

---
+++ setup.py
@@ -0,0 +1,16 @@
+from setuptools import setup, find_packages
+ 
+setup(name='django-echelon',
+    version=".".join(map(str, __import__("echelon").__version__)),
+    description='Middleware to make user information always available',
+    author='Dennis Kaarsemaker',
+    author_email='dennis@kaarsemaker.net',
+    url='http://github.com/seveas/django-echelon',
+    packages=find_packages(),
+    classifiers=[
+        "Framework :: Django",
+        "Intended Audience :: Developers",
+        "Operating System :: OS Independent",
+        "Topic :: Software Development"
+    ],
+)

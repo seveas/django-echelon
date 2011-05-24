a4e115e2e276438734f6d3cfdac68774f731d475
Atamert Ölçgen <muhuk@muhuk.com>
2011-05-24 13:06:59 +0800

    Excluded example project from the package

--- setup.py
+++ setup.py
@@ -1,12 +1,12 @@
 from setuptools import setup, find_packages
- 
+
 setup(name='django-echelon',
     version=".".join(map(str, __import__("echelon").__version__)),
     description='Middleware to make user information always available',
     author='Dennis Kaarsemaker',
     author_email='dennis@kaarsemaker.net',
     url='http://github.com/seveas/django-echelon',
-    packages=find_packages(),
+    packages=find_packages(exclude=["example"]),
     classifiers=[
         "Framework :: Django",
         "Intended Audience :: Developers",
